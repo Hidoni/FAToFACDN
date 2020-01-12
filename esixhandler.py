85e3d569100f435ab6ea2b5c3387c7dbc07f63ee
Hidoni <ron30086@walla.com>
2020-01-12 23:49:16 +0200

    Added basic e621 handler (Still needs work)

---
+++ esixhandler.py
@@ -0,0 +1,48 @@
+import requests
+import logging
+import re
+import py621
+import shutil
+
+logger = logging.getLogger(__name__)
+RATING_MAP = {'e': "Explicit", 'q': "Questionable", 's': "Safe"}
+
+
+class e621Info:
+    def __init__(self, direct_link, artist, tags, rating, sample_url):
+        self.direct_link = direct_link
+        self.artist = artist
+        self.image_name = None
+        self.tags = tags
+        try:
+            self.rating = RATING_MAP[rating]
+        except KeyError:
+            self.rating = "Unknown"
+        self.sample_url = sample_url[8:]
+        logger.debug(f"Info class created with the following values: direct_link:{direct_link}, artist_name:{artist}, tags:{tags}, rating:{rating}, sample_url:{sample_url}")
+
+    def download(self, path):
+        user_agent = "Py621/1.1 (by Hidoni on e621)"
+        try:
+            image = requests.get(self.direct_link, stream=True, headers={'User-Agent': user_agent})
+            name = path + '.' + self.direct_link.split('.')[-1]
+            with open(name, 'wb') as output_location:
+                image.raw.decode_content = True
+                shutil.copyfileobj(image.raw, output_location)
+            del image
+            return name
+        except Exception as e:
+            logger.debug("Got the following error while trying to download: {0}".format(str(e)))
+            return None
+
+
+def get(link):
+    logger.info(f"Handling URL: {link}")
+    post_id = re.findall(r"\d+", link)[1]  # e*621*.net/post/*id* which is why we take the second match
+    post = py621.get_by_id(post_id)
+    if post is None or post.file_ext in ["swf", "webm"]:
+        return None
+    try:
+        return e621Info(post.file_url, post.artist, post.tags, post.rating, post.sample_url)
+    except Exception:
+        return None
